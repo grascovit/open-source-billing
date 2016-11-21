770cac8506cf7a844c9ec6962c6b8bb67a359710
Thiago Durante Pires <thdurante@gmail.com>
2016-11-20 22:28:59 -0200

    Testes do modelo Expense

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Expense, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Expense, type: :model do
+  it { should belong_to :client }
+
+  it { should belong_to :category }
+
+  it { should belong_to :tax1 }
+
+  it { should belong_to :tax2 }
+
+  it { should belong_to :company }
 end

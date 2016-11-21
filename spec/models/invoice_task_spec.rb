dc30e3ed52e4a375bac99d4385e76e256cb5dee7
Gabriel Rascovit <gabrielrascovit@inf.ufg.br>
2016-11-21 00:31:07 -0200

    Implementado testes para o modelo InvoiceTask

--- spec/models/invoice_task_spec.rb
+++ spec/models/invoice_task_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe InvoiceTask, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe InvoiceTask, type: :model do
+  it { should belong_to :invoice }
 end

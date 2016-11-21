e1f9185b433d14e56e0a6896641d171d0a9d275f
Thiago Durante Pires <thdurante@gmail.com>
2016-11-20 22:40:08 -0200

    Testando o modelo 'Project'

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe Project, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Project, type: :model do
+  it { should belong_to :client }
+
+  it { should belong_to :manager }
+
+  it { should belong_to :company }
+
+  # it { should belong_to :user }
+
+  it { should have_many(:project_tasks).dependent(:destroy) }
+
+  it { should have_many(:logs).dependent(:destroy) }
+
+  it { should have_many(:team_members).dependent(:destroy) }
+
+  it { should have_many(:staffs) }
+
+  it { should have_many(:invoices).dependent(:destroy) }
+
+  it { should accept_nested_attributes_for :project_tasks }
+
+  it { should accept_nested_attributes_for :team_members }
 end

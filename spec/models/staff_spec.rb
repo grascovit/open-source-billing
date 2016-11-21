8ba0f830961d0f884612ad2989ba236b801a0358
Thiago Durante Pires <thdurante@gmail.com>
2016-11-21 18:57:47 -0200

    Implementado testes para o modelo Staff

--- spec/models/staff_spec.rb
+++ spec/models/staff_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Staff, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Staff, type: :model do
+  it { should belong_to :company }
+
+  it { should belong_to :user }
+
+  it { should have_many :company_entities }
+
+  it { should have_many :team_members }
+
+  it { should have_many :projects }
 end
